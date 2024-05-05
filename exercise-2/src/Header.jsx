85c264e9a4f6dc3fb61eec3b546595892a8b916b
Mixchennie0505 <mixchennie@gmail.com>
2024-05-05 15:10:14 +0700

    C1-S2-PRACTICE

---
+++ exercise-2/src/Header.jsx
@@ -0,0 +1,12 @@
+function Header() {
+  
+    return (
+      <header class="block">
+      <h1>Welcome!</h1>
+      <p></p>
+    </header>
+    )
+  }
+  
+  export default Header
+  
---
+++ exercise3/src/components/Header.jsx
@@ -0,0 +1,11 @@
+let title = "ATOMIC CLOCK"
+
+function Header() {
+    return(
+        <header>
+            <h1>{title}</h1>
+        </header>
+    );
+}
+
+export default Header;
